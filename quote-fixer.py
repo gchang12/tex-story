67119baa55877d1e02bb1a0f57999de8edfa77a0
George Chang <59218296+gchang12@users.noreply.github.com>
2022-03-08 04:43:04 -1000

    Add files via upload
    Better version of the other TeX-converting script

---
+++ quote-fixer.py
@@ -0,0 +1,72 @@
+import re
+
+from os import mkdir, walk, system
+from os.path import sep, exists
+
+from shutil import copy
+
+scene_dir='scenes'
+prose_dir='prose'
+
+def copy_files():
+    if not exists(prose_dir):
+        message='Please make a symlink to the prose directory entitled `prose\', thanks.\n'
+        print(message)
+        raise Exception
+    if not exists(scene_dir):
+        mkdir(scene_dir)
+    for dir_name,y,filelist in walk(prose_dir):
+        if dir_name != prose_dir:
+            continue
+        filelist=filelist[::-1]
+        while filelist:
+            file=filelist.pop()
+            src=sep.join([prose_dir,file])
+            file=file.replace('.txt','.tex')
+            dst=sep.join([scene_dir,file])
+            copy(src,dst)
+
+pattern_list=[
+    # Matches double-quotes followed immediately by single-quote
+    ('``\\,`','"`'),\
+    # Match double-quotes if at start of string or if it follows (white)space
+    ('``','^"'),\
+    (' ``','(?<=[^-])\s"'),\
+    ]
+
+def fix_line(line):
+    for repl,pattern in pattern_list:
+        line=re.sub(pattern,repl,line)
+    return line
+
+def fix_file(file):
+    lines=list()
+    file=sep.join([scene_dir,file])
+    with open(file) as rfile:
+        for line in rfile.readlines():
+            line=fix_line(line)
+            lines.append(line)
+    with open(file,'w') as wfile:
+        lines=lines[::-1]
+        while lines:
+            line=lines.pop()
+            wfile.write(line)
+
+def fix_story():
+    for x,y,filelist in walk(scene_dir):
+        filelist=filelist[::-1]
+        while filelist:
+            file=filelist.pop()
+            fix_file(file)
+
+def typeset_and_clone():
+    copy_files()
+    fix_story()
+    system('pdflatex -jobname=svtfoe4 main')
+    disk_drive='/media/eclair/NOOK'
+    success_message=r'\\nsvtfoe4.pdf successfully cloned to NOOK.'
+    sfmt=disk_drive,disk_drive,success_message
+    system('if [ -e %s/NOOK/ ]; then (cp svtfoe4.pdf %s/NOOK/ && echo %s); fi;'%sfmt)
+
+if __name__ == '__main__':
+    typeset_and_clone()
