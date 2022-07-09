788d483f0fed90c09a8f2100fa0035441437090f
George Chang <59218296+gchang12@users.noreply.github.com>
2022-07-08 18:02:03 -1000

    Add files via upload

--- quote-fixer.c
+++ quote-fixer.c
@@ -1,103 +1,50 @@
-/*
-Given a string: "Stuff happens." More stuff happens. "Wait --- ". "No, I do not think so."
--Replace all double-quotes that occur at the beginning with two left-quotes
--Replace all double-quotes that occur after spaces
---but only if said spaces are not preceded by em-dashes
--Replace left double-quote, single left-quote scheme by thinspace macro
-*/
+// https://www.geeksforgeeks.org/c-program-list-files-sub-directories-directory/
 
 #include <stdio.h>
+#include <stdlib.h> // To invoke "system"
+#include <dirent.h> // To walk through directories
+#include <sys/stat.h> // To make directories 
 #include <string.h>
-#include <sys/stat.h>
 
-#define TEX_DIR "./scenes/"
+#define ROOT "perl ~/Documents/coding/linux/convert-to-tex/tex-converter.pl "
 
-void fixQuotes(char *str_ptr, char *old_str){
-// Declare variable to store string with fixed quotes
-    char new_str[1024]={0};
-// New index for new string, plus variables to track em-dashes
-    int j=0,dash_count=0,after_dash=0;
-    for (int i=0; i<strlen(old_str); i++){
-        if (old_str[i] == '"'){
-            if (i == 0 || dash_count != 3 && old_str[i-1] == ' '){
-// Two scenarios explained above
-                new_str[j++]='`';
-                new_str[j++]='`';
-            }
-            else {
-// Accumulate double-quote normally otherwise
-                new_str[j++]='"';
-            }
-        }
-        else {
-// Accumulate char's normally
-            if (old_str[i] == '`' && old_str[i-1] == '"'){
-                new_str[j++]='\\';
-                new_str[j++]=',';
-                new_str[j++]='`';
-            }
-            else {
-                new_str[j++]=old_str[i];
-            }
-            if (old_str[i] == '-'){
-                dash_count++;
-            }
-            else {
-// Increment only after three consecutive dashes
-                if (dash_count == 3){
-                    after_dash++;
-                }
-/*
-If fewer than three consecutive dashes are encountered (e.g. hyphenated words and stutters), then reset dash counters
-*/
-                else {
-                    dash_count=0;
-                    after_dash=0;
-                }
-                if (after_dash == 2){
-// Reset dash counters two characters after em-dash
-                    dash_count=0;
-                    after_dash=0;
-                }
-            }
-        }
+int main(void){
+    char argv[3][8]={"","prose","scenes"};
+    // Declare the source folder.
+    DIR *src_dir=opendir(argv[1]);
+    struct dirent *de;
+    mkdir(argv[2],S_IRWXU);
+    // It's probably better that the program raises an error if the target directory already exists. As it so happens, the system goes on uninterrupted even if the directory exists.
+    char *s;
+    char cmd[256];
+    char perl_args[7][64] = {[1]="/",""," ",[5]="/",""};
+    int file_indices[4]={2,6,0,4};
+// Copying argv[1] and argv[2] to their places as indicated
+    for (int i=2; i<4; i++)
+        strcat(perl_args[file_indices[i]],argv[i-1]);
+    while ((de=readdir(src_dir)) != NULL) {
+        s=de->d_name;
+// Matching '.' and '..' in the "walk" command
+        if ((strlen(s) == 1 && s[0] == '.') || (strlen(s) == 2 && s[0] == '.' && s[1] == '.'))
+            continue;
+        strcpy(cmd,ROOT);
+        for (int j=0; j<2; j++)
+            strcpy(perl_args[file_indices[j]],s);
+        for (int j=0; j<7; j++)
+            strcat(cmd,perl_args[j]);
+
+// Converting the extensions of the output files to "tex", assuming that the source files have "txt" extensions
+        cmd[strlen(cmd)-2]='e';
+        cmd[strlen(cmd)-1]='x';
+        //printf("%s\n",cmd);
+        system(cmd);
     }
-// Copy new string to pointer variable
-    strcpy(str_ptr,new_str);
-}
-
-void changeExt(char *wpath){
-// Manually changing the extension of a string because I don't know how to use string methods
-    wpath[strlen(wpath)-2]='e';
-    wpath[strlen(wpath)-1]='x';
-}
-
-void rewriteFile(char *dest){
-// For fixQuotes call
-    char old_str[1024],str_ptr[1024];
-// Write destination
-    char wpath[32]=TEX_DIR;
-// Concatenate filename to write-directory
-    strcat(wpath,strrchr(dest,'/')+1);
-    changeExt(wpath);
-    FILE *fp,*fq;
-    fp=fopen(dest,"r");
-    fq=fopen(wpath,"w");
-// Process string and write string here
-    while (fgets(old_str,sizeof(old_str),fp) != NULL){
-        fixQuotes(str_ptr,old_str);
-        fputs(str_ptr,fq);
-    }
-    fclose(fp);
-    fclose(fq);
-    return;
-}
-
-int main(int argc, char **argv){
-    mkdir(TEX_DIR,S_IRWXU);
-// Change every file in the argument list
-    for (int i=1; i < argc; i++){
-        rewriteFile(argv[i]);
-    }
+    closedir(src_dir);
+    char *tcmd="pdflatex -jobname dark-eclipsa_arc main";
+    char typeset[64];
+    strcpy(typeset,tcmd);
+    strcat(typeset," && ");
+    strcat(typeset,tcmd);
+    system(typeset);
     return 0;
 }
