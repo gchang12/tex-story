08aefa0760d6660e199b0b5c86606e773b89d72f
George Chang <59218296+gchang12@users.noreply.github.com>
2022-07-08 21:44:42 -1000

    Update sh_quote-fixer.sh

--- sh_quote-fixer.sh
+++ sh_quote-fixer.sh
@@ -3,5 +3,5 @@ cmd="pdflatex main";
 src="prose";
 tgt="scenes"
 cp -r "$src" "$tgt";
-for i in $(ls "$src"); do perl ~/Documents/coding/linux/convert-to-tex/tex-converter.pl "$src/$i" "$tgt/${i/.txt/.tex}"; done;
+for i in $(ls "$src"); do perl tex-converter.pl "$src/$i" "$tgt/${i/.txt/.tex}"; done;
 "$cmd" && "$cmd";
